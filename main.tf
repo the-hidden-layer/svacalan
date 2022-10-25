c5e4390d90c05f0dfb629f3206c8f49a2a8e73a8
astrlux <37908451+astrlux@users.noreply.github.com>
2022-10-25 11:50:10 -0700

    Test EBS volume attachment

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "aws_vpc" "test-env" {
   enable_dns_support   = true
 }
 
-resource "aws_subnet" "subnet-uno" {
+resource "aws_subnet" "subnet-1" {
   # creates a subnet
   cidr_block        = cidrsubnet(aws_vpc.test-env.cidr_block, 3, 1)
   vpc_id            = aws_vpc.test-env.id
@@ -12,7 +12,7 @@ resource "aws_subnet" "subnet-uno" {
   depends_on        = [aws_internet_gateway.test-env-gw]
 }
 
-resource "aws_security_group" "ingress-ssh-test" {
+resource "aws_security_group" "ingress-ssh" {
   name   = "allow-ssh-sg"
   vpc_id = aws_vpc.test-env.id
 
@@ -34,29 +34,7 @@ resource "aws_security_group" "ingress-ssh-test" {
   }
 }
 
-resource "aws_security_group" "ingress-http-test" {
-  name   = "allow-http-sg"
-  vpc_id = aws_vpc.test-env.id
-
-  ingress {
-    cidr_blocks = [
-      "0.0.0.0/0"
-    ]
-
-    from_port = 80
-    to_port   = 80
-    protocol  = "tcp"
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group" "ingress-https-test" {
+resource "aws_security_group" "ingress-https" {
   name   = "allow-https-sg"
   vpc_id = aws_vpc.test-env.id
 
@@ -79,9 +57,10 @@ resource "aws_security_group" "ingress-https-test" {
 }
 
 resource "aws_eip" "ip-test-env" {
-  instance   = aws_spot_instance_request.test_worker.spot_instance_id
-  vpc        = true
-  depends_on = [aws_internet_gateway.test-env-gw]
+  instance = aws_spot_instance_request.test_worker.spot_instance_id
+  vpc      = true
+  depends_on = [aws_internet_gateway.test-env-gw,
+  aws_spot_instance_request.test_worker]
 }
 
 resource "aws_internet_gateway" "test-env-gw" {
@@ -98,7 +77,7 @@ resource "aws_route_table" "route-table-test-env" {
 }
 
 resource "aws_route_table_association" "subnet-association" {
-  subnet_id      = aws_subnet.subnet-uno.id
+  subnet_id      = aws_subnet.subnet-1.id
   route_table_id = aws_route_table.route-table-test-env.id
 }
 
@@ -107,15 +86,23 @@ resource "aws_key_pair" "spot_key" {
   public_key = file("/Users/astrlux/Downloads/public_key.pem")
 }
 
-resource "aws_spot_instance_request" "test_worker" {
+resource "aws_spot_instance_request" "scheduler" {
   ami                  = "ami-830c94e3"
-  spot_price           = "0.014"
   instance_type        = "t2.micro"
+  spot_price           = "0.014"
   spot_type            = "one-time"
   wait_for_fulfillment = "true"
-  key_name             = "spot_key"
 
-  security_groups = ["${aws_security_group.ingress-ssh-test.id}", "${aws_security_group.ingress-http-test.id}",
-  "${aws_security_group.ingress-https-test.id}"]
-  subnet_id = aws_subnet.subnet-uno.id
+  key_name             = "spot_key"
+  security_groups = [
+    aws_security_group.ingress-ssh.id,
+    aws_security_group.ingress-https.id
+  ]
+  subnet_id = aws_subnet.subnet-1.id
+
+  ebs_block_device {
+    device_name = "/dev/sda1"
+    volume_size = 20
+    volume_type = "gp2"
+  }
 }
